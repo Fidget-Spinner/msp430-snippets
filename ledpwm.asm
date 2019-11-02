48a108d1ba57545a2f00e8f6166474f79f3687df
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2019-11-02 14:08:56 +0800

    fixed more formatting

--- ledpwm.asm
+++ ledpwm.asm
@@ -30,20 +30,20 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 			sub.w	R11, R10				; Counter for Delay loop; 4600 counts is 1 cycle.
 			bis.b	#001h, &P1DIR				; Set P1.0 as output
-Start		mov.w	#2,R9						; Counter for on/off state. on = 1, off = 2
+Start			mov.w	#2,R9					; Counter for on/off state. on = 1, off = 2
 
 
-Mainloop	xor.b	#001h, &P1OUT          				; Toggle value of P1.0 (ie toggle the LED)
+Mainloop		xor.b	#001h, &P1OUT          			; Toggle value of P1.0 (ie toggle the LED)
 			mov.w	R10, R15				; Resets counter for Delay loop
 
 			cmp.w	#2,R9					; Test if state is on or off, off = 2 will set neg flag
-			jeq		Delay				; If eq (off), jmp to Delay
+			jeq	Delay					; If eq (off), jmp to Delay
 			mov.w	R11, R15				; Else, (on), will mov R11 (counter for on) into the R15 counter
 
-Delay		dec.w	R15						; The Delay loop
-			jnz		Delay				; While R15 is not zero, keep looping back to label Delay
+Delay			dec.w	R15					; The Delay loop
+			jnz	Delay					; While R15 is not zero, keep looping back to label Delay
 			dec.w	R9
-			jz		Start				; Marks the end of 1 clock cycle
+			jz	Start					; Marks the end of 1 clock cycle
 			jmp 	Mainloop
 			nop
 
