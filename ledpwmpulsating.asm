290527bc440ac2e191fa560b9297eb64b70a5974
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2019-11-02 14:19:30 +0800

    more formatting fixes

--- ledpwmpulsating.asm
+++ ledpwmpulsating.asm
@@ -37,22 +37,22 @@ RetToggle   cmp.w   #0, R6		    ; If counting up,
 	    add.w   #16, R11		    ; Else add 4 minus 2 (add 2 in total)
 Down	    sub.w   #8, R11		    ; If counting down, minus 2
 	    mov.w   #4600d,R10		    ; 4600 counts is 1 cycle
-	    sub.w   R11, R10				        ; Counter for Delay loop; 4600 counts is 1 cycle.
+	    sub.w   R11, R10		    ; Counter for Delay loop; 4600 counts is 1 cycle.
 
 Mainloop    xor.b   #001h, &P1OUT           ; Toggle value of P1.0 (ie toggle the LED)
-	    mov.w   R10, R15				        ; Resets counter for Delay loop
-            cmp.w   #2,R9					          ; Test if state is on or off, off = 2 will set neg flag
-            jeq     Delay					          ; If eq(off), jmp to Delay
-            mov.w   R11, R15				        ; Else, (on), will mov R11 (counter for on) into the R15 counter
+	    mov.w   R10, R15		    ; Resets counter for Delay loop
+            cmp.w   #2,R9		    ; Test if state is on or off, off = 2 will set neg flag
+            jeq     Delay		    ; If eq(off), jmp to Delay
+            mov.w   R11, R15		    ; Else, (on), will mov R11 (counter for on) into the R15 counter
 
-Delay	    dec.w   R15						          ; The Delay loop
-            jnz     Delay					          ; While R15 is not zero, keep looping back to label Delay
+Delay	    dec.w   R15			    ; The Delay loop
+            jnz     Delay		    ; While R15 is not zero, keep looping back to label Delay
             dec.w   R9
-            jz      Start					          ; Marks the end of 1 clock cycle
+            jz      Start		    ; Marks the end of 1 clock cycle
             jmp     Mainloop
 
-Toggle      xor.w   #001h,R6				        ; Toggle up or down
-            mov.w   #0h, R5					        ; Reset the period counter
+Toggle      xor.w   #001h,R6		    ; Toggle up or down
+            mov.w   #0h, R5		    ; Reset the period counter
             jmp     RetToggle
             nop
 ;-------------------------------------------------------------------------------
