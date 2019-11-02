668477c912b2c6b08151927da0379209a4886717
Fidget-Spinner <28750310+Fidget-Spinner@users.noreply.github.com>
2019-11-02 14:08:05 +0800

    fixed some formatting

--- ledpwm.asm
+++ ledpwm.asm
@@ -29,21 +29,21 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 			mov.w	#2d,R11					; R11 stores counts to keep light on for, change this value to change duty cycle
 
 			sub.w	R11, R10				; Counter for Delay loop; 4600 counts is 1 cycle.
-			bis.b	#001h, &P1DIR			; Set P1.0 as output
-Start		mov.w	#2,R9					; Counter for on/off state. on = 1, off = 2
+			bis.b	#001h, &P1DIR				; Set P1.0 as output
+Start		mov.w	#2,R9						; Counter for on/off state. on = 1, off = 2
 
 
-Mainloop	xor.b	#001h, &P1OUT           ; Toggle value of P1.0 (ie toggle the LED)
+Mainloop	xor.b	#001h, &P1OUT          				; Toggle value of P1.0 (ie toggle the LED)
 			mov.w	R10, R15				; Resets counter for Delay loop
 
-			cmp.w	#2,R9					; Test if state is on or off, off = 1 will set neg flag
-			jeq		Delay					; If neg (off), jmp to Delay
+			cmp.w	#2,R9					; Test if state is on or off, off = 2 will set neg flag
+			jeq		Delay				; If eq (off), jmp to Delay
 			mov.w	R11, R15				; Else, (on), will mov R11 (counter for on) into the R15 counter
 
 Delay		dec.w	R15						; The Delay loop
-			jnz		Delay					; While R15 is not zero, keep looping back to label Delay
+			jnz		Delay				; While R15 is not zero, keep looping back to label Delay
 			dec.w	R9
-			jz		Start					; Marks the end of 1 clock cycle
+			jz		Start				; Marks the end of 1 clock cycle
 			jmp 	Mainloop
 			nop
 
